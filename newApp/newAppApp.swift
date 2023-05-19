b0d5fca0b0d8cf0d08d9be51f57358fb7e7a7d21
Margo <zaleskayamargarita@gmail.com>
2023-05-20 00:56:19 +0200

    Initial Commit

---
+++ newApp/newAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAppApp.swift
+//  newApp
+//
+//  Created by Margarita Zaleskaya on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct newAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
